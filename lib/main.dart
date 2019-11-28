255f3da27a0cd995ae0fac71056a9b1dc16a98c6
Damola Adekoya <adekoyafelix@gmail.com>
2019-11-28 09:50:45 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  StreamController _streamController ;
+  Stream _stream;
+
+
+  @override
+  void initState() {
+    _streamController = StreamController();
+    _stream= _streamController.stream;
+
+  }
+
+  void _incrementCounter() {
+  _streamController.sink.add(_counter++);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+           StreamBuilder(
+             stream: _stream,
+             builder: (BuildContext context, AsyncSnapshot snapshot){
+
+               return  Text(
+               snapshot.data!=null ? snapshot.data.toString() : "0",
+                 style: Theme.of(context).textTheme.display1,
+               );
+             }
+           )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  @override
+  void dispose() {
+
+  }
+}
